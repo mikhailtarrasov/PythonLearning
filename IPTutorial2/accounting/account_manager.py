3bc658b86c363e0de84cb5e20adb757344d3f9c7
Mikhail Tarasov <mikhailtarrasov@gmail.com>
2018-01-31 23:01:27 +0500

    income/expenses account_manager refactoring

--- IPTutorial2/accounting/account_manager.py
+++ IPTutorial2/accounting/account_manager.py
@@ -8,34 +8,31 @@ class AccountManager(object):
     def __init__(self):
         self.__accounts = []  # type: Account
 
-    def is_account_exist(self, account_name):
+    def __is_account_exist(self, account_name):
         if account_name is not None:
             return account_name in (account.name for account in self.__accounts)
         else:
             return False
 
     def add_account(self, account_name):
-        msg = ''
-        if account_name is None:
-            return None
-        if not self.is_account_exist(account_name):
+        if not self.__is_account_exist(account_name):
             new_account = Account(account_name)
             self.__accounts.append(new_account)
-            msg = 'Account {0} was successfully added'.format(account_name)
+            msg = 'Account \'{0}\' was successfully added'.format(account_name)
         else:
             msg = 'Warning! Action canceled, account with the same name already exist.'
         return msg
 
     def change_account(self, account_name):
-        if self.is_account_exist(account_name):
+        if self.__is_account_exist(account_name):
             self.__current_account_name = account_name
-            msg = 'Account {0} was successfully set active!'.format(account_name)
+            msg = 'Account \'{0}\' was successfully set active!'.format(account_name)
         else:
-            msg = 'Error! Account with "{0}" name is not exist yet!'.format(account_name)
+            msg = 'Error! Account with \'{0}\' name is not exist yet!'.format(account_name)
         return msg
 
     def __get_account(self, account_name=__current_account_name):
-        if self.is_account_exist(account_name):
+        if self.__is_account_exist(account_name):
             return None
         else:
             return (account for account in self.__accounts if account.name == account_name)
@@ -45,7 +42,7 @@ class AccountManager(object):
         if current_account is not None:
             return current_account.balance
         else:
-            msg = 'Error! Account with "{0}" name is not exist yet!'.format(self.__current_account_name)
+            msg = 'Error! Account with \'{0}\' name is not exist yet!'.format(self.__current_account_name)
             return msg
 
     def show_accounts(self):
