56943675be0bcd993f5e9a756ea2b962448c1a95
Mikhail Tarasov <mikhailtarrasov@gmail.com>
2018-02-05 23:09:07 +0500

    income/expenses Account refactoring

--- IPTutorial2/accounting/account.py
+++ IPTutorial2/accounting/account.py
@@ -1,16 +1,13 @@
 class Account(object):
     name = ""
-    balance = None
+    balance = None  # type: float
 
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
 
-    def income(self, income):
+    def add_transaction(self, income):
         self.balance = self.balance + income
-
-    def expenses(self, expenses):
-        self.balance = self.balance - expenses
 
     def get_balance(self):
         return self.balance
